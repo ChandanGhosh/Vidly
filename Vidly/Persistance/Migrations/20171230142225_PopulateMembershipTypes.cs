74b366b3c64463a5f99f64000e46c7aab24315e2
chandanghosh <ckghosh1983@gmail.com>
2017-12-30 20:00:16 +0530

    csproj formatting error

---
+++ Vidly/Persistance/Migrations/20171230142225_PopulateMembershipTypes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Vidly.Persistance.Migrations
+{
+    public partial class PopulateMembershipTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO MembershipTypes(Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(1, 0, 0, 0)"); // Id=1 PayAsyouGo
+
+            migrationBuilder.Sql(
+                "INSERT INTO MembershipTypes(Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(2, 30, 1, 10)"); // Id=2 Monthly
+            migrationBuilder.Sql(
+                "INSERT INTO MembershipTypes(Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(3, 90, 3, 15)"); // Id=3 Quarterly
+            migrationBuilder.Sql(
+                "INSERT INTO MembershipTypes(Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(4, 300, 12, 20)"); // Id=2 Yearly
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
